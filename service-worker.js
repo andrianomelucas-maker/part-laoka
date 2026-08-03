d620633ceebbd5ed487b03d17092f926604b1480
andrianomelucas-maker <andrianomelucas@gmail.com>
2026-08-03 23:03:20 +0300

    Fix CACHE_NAME typo in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kajin-daoko-v4';
+const CACHE_NAME = 'kajin-daoka-v4';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './bg-river.jpg'];
 
 self.addEventListener('install', event => {
